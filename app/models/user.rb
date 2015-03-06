f387b5aef0f7f5b5cc17ace01aec5bffd3ff7d2b
Peter Lord <pmlord@gmail.com>
2015-03-06 18:36:10 -0500

    added gravatar

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,10 @@ class User < ActiveRecord::Base
   validates :email, format: Regex.email
   validates_confirmation_of :password
   validates_presence_of :password_confirmation, if: :crypted_password_changed?
+  
+  
+  def avatar_url(size)
+    Avatar.gravatar(email, size)
+  end
+  
 end
---
+++ lib/avatar.rb
@@ -0,0 +1,20 @@
+module Avatar
+  
+  SIZES = { xl: 400, lg: 200, md: 80, sm: 30, xs: 20 }
+  
+  def self.pixel_size(size)
+    SIZES[size]
+  end
+  
+  def self.pixel_size_str(size)
+    "#{SIZES[size]}px"
+  end
+  
+  def self.gravatar(email_address, size)
+    email_address.downcase!
+    hash = Digest::MD5.hexdigest(email_address)
+    px = size.is_a?(Symbol) ? pixel_size(size) : size
+    "http://www.gravatar.com/avatar/#{hash}?s=#{px}"
+  end
+  
+end
